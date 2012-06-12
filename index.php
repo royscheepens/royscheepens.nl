baa21c892ba3a044b12351a9ceb8db1be8ea752a
Roy Scheepens <roy.scheepens@gmail.com>
2012-06-12 17:54:23 +0200

    First draft

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Roy Scheepens - Multimedia Web Developer</title>
+
+	<meta name="revisit-after" content="1 month" />
+	<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width">
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet' type='text/css'>
+
+	<style type="text/css">
+
+		/* https://github.com/vladocar/CSS-Mini-Reset */
+		html,body,div,form,fieldset,legend,label{margin:0;padding:0;}table{border-collapse:collapse;border-spacing:0;}th,td{text-align:left;vertical-align:top;}h1,h2,h3,h4,h5,h6,th,td,caption{font-weight:normal;}img{border:0;}
+
+		body {
+			font-size:1em;
+			line-height:1.875em;
+			text-align:left;
+
+			font: 300 1.3em/1.875em 'Open Sans', Helvetica, Arial, sans-serif;
+			color:#555;
+		}
+
+		article {
+			display:block;
+			margin: 6em auto 4em;
+			max-width:700px;
+		}
+
+		figure {
+			display:block;
+			margin:0 0 20px 0;
+		}
+
+		figure img { 
+			-webkit-border-radius: 180px;
+			   -moz-border-radius: 180px;
+			        border-radius: 180px;
+		}
+
+		p { }
+
+		a {
+			color:#d0a884;
+			text-decoration:none;
+			-webkit-transition: color .2s ease;
+			   -moz-transition:  color .2s ease;
+			        transition:  color .2s ease;		
+		}
+
+		a:hover {
+			color:#b19479;
+		}
+
+		a:active {}
+
+
+		@media only screen and (max-width: 500px) {
+			article {
+				margin:2em auto;
+				padding:0 1.2em;
+			}
+		}
+
+		@media only screen and (max-height: 600px) {
+			article {
+				margin:2em auto;
+			}
+		}
+
+	</style>
+
+	<script type="text/javascript">
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-2524420-1']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	</script>
+
+</head>
+<body>
+
+	<article>
+		
+		<figure>
+			<img src="http://www.gravatar.com/avatar/<?php echo strtolower(trim(md5('roy.scheepens@gmail.com'))) ?>?s=180" alt="This is me!">
+		</figure>
+
+		<p>I'm a Dutch digital media developer and web entrepreneur. </p>
+
+		<p>I like to dabble in both front- and backend technologies. Currently my frameworks of choice are <a href="http://www.codeigniter.com">CodeIgniter</a> and <a href="http://laravel.com/">Laravel</a>, spiced up with a bit of <a href="http://www.lesscss.org">LESS</a> and <a href="http://www.jquery.com">jQuery</a>.</p>
+
+		<p>Check me out on <a href="http://nl.linkedin.com/in/royscheepens">LinkedIn</a>, <a href="https://github.com/royscheepens">Github</a>, <a href="https://forrst.com/people/royscheepens">Forrst</a> and <a href="http://open.spotify.com/user/1112904683">Spotify</a>. Please don't <a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#114;&#111;&#121;&#46;&#115;&#99;&#104;&#101;&#101;&#112;&#101;&#110;&#115;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;'>email me</a>.</p>
+
+	</article>
+
+</body>
+</html>
