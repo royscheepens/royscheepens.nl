2ae6806f060e6d647648ac976999a51cacded9ba
Roy Scheepens <roy.scheepens@gmail.com>
2013-06-15 23:58:24 +0200

    Added portfolio, updated intro

--- index.php
+++ index.php
@@ -14,38 +14,33 @@
 		/* https://github.com/vladocar/CSS-Mini-Reset */
 		html,body,div,form,fieldset,legend,label{margin:0;padding:0;}table{border-collapse:collapse;border-spacing:0;}th,td{text-align:left;vertical-align:top;}h1,h2,h3,h4,h5,h6,th,td,caption{font-weight:normal;}img{border:0;}
 
+		/* http://nicolasgallagher.com/micro-clearfix-hack/ */
+		.cf:before, .cf:after { content: " "; display: table; } .cf:after { clear: both; } .cf { *zoom: 1; }
+
+		html { overflow-y:scroll; }
+
 		body {
 			font-size:1em;
-			line-height:1.875em;
-			text-align:left;
+			font-size:1rem;
 
+			line-height:1.875em;
+			line-height:1.875rem;
+
+			text-align:left;
 			font: 300 1.3em/1.875em 'Open Sans', Helvetica, Arial, sans-serif;
-			color:#555;
+			color:#666;
 		}
 
 		article {
 			display:block;
-			margin: 6em auto 4em;
+			margin: 4em auto;
 			max-width:700px;
 			padding:0 1em;
 		}
 
 		article > * { display:none; }
 
-		figure {
-			display:none;
-			margin:0 0 20px 0;
-		}
-
-		figure img { 
-			-webkit-border-radius: 180px;
-			   -moz-border-radius: 180px;
-			        border-radius: 180px;
-		}
-
-		p { }
-
-		a {
+		a, a:visited {
 			color:#d0a884;
 			text-decoration:none;
 			-webkit-transition: color .2s ease;
@@ -53,29 +48,119 @@
 			        transition:  color .2s ease;		
 		}
 
-		em.red { font-style:normal; color:#900; }
-
-		a:hover {
-			color:#b19479;
-		}
-
+		a:hover { color:#b19479; }
 		a:active {}
 
+		em.red { font-style:normal; color:#900; }
 
-		@media only screen and (max-width: 500px) {
+		@media only screen and (max-height: 600px) {
+
 			body {
 				font-size:1.1em;
+				font-size:1.1rem;
 			}
+
+
 			article {
-				margin:2em auto;
-				padding:0 1.2em;
+				margin:3em auto;
 			}
 		}
 
-		@media only screen and (max-height: 600px) {
-			article {
-				margin:2em auto;
-			}
+		/**
+		 *	Intro
+		 */
+
+		.intro figure {
+			display:none;
+			margin:0 0 1em 0;
+		}
+
+		.intro figure img { 
+			-webkit-border-radius: 180px;
+			   -moz-border-radius: 180px;
+			        border-radius: 180px;
+		}
+
+		.intro .availability {
+			font-size:.7em;
+			color:#75ac45;
+		}
+
+		@-webkit-keyframes grow {
+			0% { background: #9cca74; /*-webkit-box-shadow: 0 0 0 #75ac45;*/ }
+			100% { background: #75ac45; /*-webkit-box-shadow: 0 0 12px #75ac45;*/ }
+		}
+
+		@-mox-keyframes grow {
+			0% { background: #9cca74; /*-mox-box-shadow: 0 0 0 #75ac45;*/ }
+			100% { background: #75ac45; /*-mox-box-shadow: 0 0 12px #75ac45;*/ }
+		}
+
+		@keyframes grow {
+			0% { background: #9cca74; /*box-shadow: 0 0 0 #75ac45;*/ }
+			100% { background: #75ac45; /*box-shadow: 0 0 12px #75ac45;*/ }
+		}
+
+		.intro .indicator {
+			display:inline-block;
+
+			width:12px;
+			height:12px;
+
+			position:relative;
+			top:1px;
+			
+			-webkit-border-radius: 100%;
+		       -moz-border-radius: 100%;
+		            border-radius: 100%;
+
+		    margin-right:.2em;
+
+			background: #75ac45;
+			-webkit-animation: grow ease-in-out infinite alternate 1s;
+			-moz-animation: grow ease-in-out infinite alternate 1s;
+			animation: grow ease-in-out infinite alternate 1s;
+
+		}
+
+		/**
+		 *	Portfolio
+		 */
+
+		.portfolio h1 { font-weight:300; border-top:1px solid #dedede; }
+
+		.portfolio h1 span {
+			position:relative;
+			top:-0.78em;
+			background:#fff;
+			padding-right:.78em;
+		}
+
+		.portfolio .item { font-size: 72%; line-height:1.6em; margin-bottom:6em; }
+
+		.portfolio .item p { margin-top:0; color:#999; }
+		.portfolio .item h2 { font-weight:300; margin-bottom:0.6em; }
+		.portfolio .item h2 span { font-size: 0.6em; color:#999; float:right;  }
+		.portfolio .item h3 { font-weight:300; margin-bottom:0.6em; }
+
+		.portfolio .item figure { 
+			text-align:center;
+			width:100%;
+			margin:0; padding:0;
+			
+		}
+
+		.portfolio .item figure img {
+			display:block;
+			margin:0 auto;
+		}
+
+		@media only screen and (max-width: 500px) {
+
+			.portfolio .item figure img {
+				max-width:100%;
+			} 
+
 		}
 
 	</style>
@@ -95,19 +180,70 @@
 </head>
 <body>
 
-	<article>
+	<article class="intro">
 		
 		<figure>
 			<img src="http://www.gravatar.com/avatar/<?php echo strtolower(trim(md5('roy.scheepens@gmail.com'))) ?>?s=180" alt="This is me!">
 		</figure>
 
-		<p>I'm a Dutch digital media developer and web entrepreneur.</p>
+		<p>I'm an all Dutch designer, developer and web entrepreneur.</p>
 
-		<p>I like to dabble in both front- and backend technologies. Currently my frameworks of choice are <a href="http://www.codeigniter.com">CodeIgniter</a> and <a href="http://laravel.com/">Laravel</a>, spiced up with a bit of <a href="http://www.lesscss.org">LESS</a> and <a href="http://www.jquery.com">jQuery</a>.</p>
+		<p>As it comes to design, I truly believe less is more. Design should be functional and beautiful, and always provide an optimal user experience. No matter which device is used.</p>
 
-		<p>Get in touch on <a href="http://nl.linkedin.com/in/royscheepens">LinkedIn</a>, <a href="https://github.com/royscheepens">Github</a>, <a href="https://forrst.com/people/royscheepens">Forrst</a> and <a href="http://open.spotify.com/user/1112904683">Spotify</a>. Please don't <a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#104;&#101;&#108;&#108;&#111;&#64;&#114;&#111;&#121;&#115;&#99;&#104;&#101;&#101;&#112;&#101;&#110;&#115;&#46;&#110;&#108;'>email me</a>.</p>
+		<p>I like to dabble in both front- and backend technologies. Currently my frameworks of choice are (but not limited to) <a href="http://www.codeigniter.com">CodeIgniter</a> and <a href="http://backbonejs.org/">Backbone.js</a>, spiced up with a bit of <a href="http://www.lesscss.org">LESS</a> and <a href="http://gruntjs.com/">Grunt</a>.</p>
+
+		<p>Like to know more? Get in touch on <a href="http://nl.linkedin.com/in/royscheepens">LinkedIn</a>, <a href="https://github.com/royscheepens">Github</a>, <a href="https://forrst.com/people/royscheepens">Forrst</a> and <a href="http://open.spotify.com/user/1112904683">Spotify</a>. Or get oldskool and <a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#104;&#101;&#108;&#108;&#111;&#64;&#114;&#111;&#121;&#115;&#99;&#104;&#101;&#101;&#112;&#101;&#110;&#115;&#46;&#110;&#108;'>email me</a>.</p>
+
+		<div class="availability">
+			<div class="indicator"></div> Currently accepting new projects
+		</div>
 
 	</article>
+
+	<article class="portfolio">
+		
+		<h1><span>See my work</span></h1>
+
+		<div class="item">
+			
+			<figure>
+				<img src="img/boels.png" alt="Boels Rental">
+			</figure>
+	
+			<h2>Boels Rental <span>2011 - present</span></h2>
+
+			<p>For one of the largest rental companies of Europe I designed and developed their online presence, both for desktop and mobile devices. Credits go to <a href="nl.linkedin.com/in/sjoerdheld">Sjoerd Held</a> for his excellent Mobile App design.</p>
+
+			<h3>What I did</h3>
+			<p>Website Design and Development, Mobile Web / Android App Development</p>
+
+			<a href="http://boels.nl">Visit the site</a> or <a href="http://itunes.com/app/boels">download the App</a>.
+
+		</div>
+
+		<p>More coming soon...</p>
+
+		<?php /* ?>
+		<div class="item">
+			
+			<figure>
+				<!-- <img src="img/boels.png" alt="Boels Rental"> -->
+			</figure>
+	
+			<h2>Another project <span>2013</span></h2>
+
+			<p>For one of the largest rental companies of Europe I designed and developed their online presence, both on desktop and mobile devices. Kodus go to <a href="nl.linkedin.com/in/sjoerdheld">Sjoerd Held</a> for his excellent Mobile App design.</p>
+
+			<h3>What I did</h3>
+			<p>About</p>
+
+			<a href="http://boels.nl">Visit the site</a>
+
+		</div>
+		<?php */ ?>
+	
+	</article>
+
 
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 	<script type="text/javascript">
